399f9cc86715cc9f96c602471e5a9f83fae0c8f7
Naired <92755796+Naired01@users.noreply.github.com>
2025-07-23 14:18:59 -0700

    Added sections to complete 🎨
    + Add Carrousel and modal
    + links

--- scrips.js
+++ scrips.js
@@ -23,3 +23,29 @@ const audioControl = () => {
 play.addEventListener('click', audioControl);
 pause.addEventListener('click', audioControl);
 
+// Carrousel imagen open
+const carrouselItems = document.querySelectorAll('.carrousel-item');
+carrouselItems.forEach(item => {
+    item.addEventListener('click', () => {
+        // get the image source from data-id attribute
+        const imageId = item.getAttribute('data-id');
+        const imageSrc = `/resourses/carrousel/${imageId}.webp`;
+        const modalImage = document.getElementById('modal-image');
+        modalImage.src = imageSrc;
+        const modal = document.getElementById('image-modal');
+        modal.style.display = 'block';
+    });
+});
+
+// Close modal image
+const modal = document.getElementById('image-modal');
+const close = document.getElementsByClassName('close')[0];
+close.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
+// Close modal when clicking outside the image
+window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+});
